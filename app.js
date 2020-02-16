1873c86b11b4659493c59f15f568ea6191afc35d
349652743 <52067234+349652743@users.noreply.github.com>
2020-02-16 18:26:19 +0800

    update chatroom

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const Koa = require('Koa');
 const path = require('path');
+const WebSocket = require('ws');
 const bodyParser = require('koa-bodyparser');
 const isProduction = process.env.NODE_ENV === 'production';
 var app = new Koa();
@@ -18,5 +19,26 @@ if (! isProduction) {
 app.use(bodyParser());
 app.use(controller());
 
-app.listen(3000);
+const server =app.listen(3000);
+
+
+// 引用Server类:
+const WebSocketServer = WebSocket.Server;
+
+// 实例化:
+const wss = new WebSocketServer({
+    server: server
+});
+wss.on('connection',function (ws,req) {
+    console.log(`[SERVER] connection()`);
+    ws.on('message', function (message) {
+        console.log(`[SERVER] Received: ${message}`);
+        wss.clients.forEach(function(client){
+            var number = wss.clients.size;
+            var data = `当前在线人数为${number}人`;
+            // console.log(message);
+            client.send( message);
+        })
+    })
+});
 console.log('app started at port 3000...');
--- controllers/hello.js
+++ controllers/hello.js
@@ -1,11 +1,10 @@
 const nunjucks = require('nunjucks');
 const isProduction = process.env.NODE_ENV === 'production';
 var fn_hello = async (ctx, next) => {
-    var name = ctx.params.name;
     nunjucks.configure('./views', { autoescape: true ,noCache : !isProduction });
-    ctx.response.body = nunjucks.render('hello.html', { name: 'fucker' });
+    ctx.response.body = nunjucks.render('hello.html');
 };
 
 module.exports = {
-    'GET /hello/:name': fn_hello
+    'GET /hello/chat': fn_hello
 };
